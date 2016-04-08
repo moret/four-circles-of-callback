225df865446432cf30c26dcc1040078fa593a4a5
Danilo Moret <github@moret.pro.br>
2016-04-08 12:12:24 -0300

    Erring along the way

--- index.js
+++ index.js
@@ -4,20 +4,39 @@ import http from 'http';
 function get(path, cb) {
   http.get(`http://localhost:3000/${path}`, response => {
     let body = '';
+    if (response.status != 200) {
+      cb('http error');
+      return;
+    }
     response.on('data', chunk => {
       body += chunk;
     });
     response.on('end', () => {
       const result = JSON.parse(body);
-      cb(result);
+      cb(undefined, result);
     });
   });
 }
 
-get('user', user => {
+get('userxxx', (err, user) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
   fs.readFile(`${user.name}.json`, (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
     const details = JSON.parse(data);
-    get(`reports/${user.name}?secret=${details.secret}`, result => {
+    get(`reports/${user.name}?secret=${details.secret}`, (err, result) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+
       console.log(result);
     });
   });
