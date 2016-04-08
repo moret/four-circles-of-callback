8c9bc8d5e17806db89de2122090228c899574b7e
Danilo Moret <github@moret.pro.br>
2016-04-08 12:08:20 -0300

    Going rounds

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 import fs from 'fs';
 import http from 'http';
 
-http.get('http://localhost:3000/user', response => {
-  let body = '';
-  response.on('data', chunk => {
-    body += chunk;
+function get(path, cb) {
+  http.get(`http://localhost:3000/${path}`, response => {
+    let body = '';
+    response.on('data', chunk => {
+      body += chunk;
+    });
+    response.on('end', () => {
+      const result = JSON.parse(body);
+      cb(result);
+    });
   });
-  response.on('end', () => {
-    const user = JSON.parse(body);
-    fs.readFile(`${user.name}.json`, (err, data) => {
-      const details = JSON.parse(data);
-      http.get(`http://localhost:3000/reports/${user.name}?secret=${details.secret}`, response => {
-        let body = '';
-        response.on('data', chunk => {
-          body += chunk;
-        });
-        response.on('end', () => {
-          const result = JSON.parse(body);
-          console.log(result);
-        });
-      });
+}
+
+get('user', user => {
+  fs.readFile(`${user.name}.json`, (err, data) => {
+    const details = JSON.parse(data);
+    get(`reports/${user.name}?secret=${details.secret}`, result => {
+      console.log(result);
     });
   });
 });
