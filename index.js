c1fbdbdda4fa69f67693f49ac8362e95ed08421e
Danilo Moret <github@moret.pro.br>
2016-04-08 12:55:31 -0300

    Liberate all!

--- index.js
+++ index.js
@@ -1,43 +1,15 @@
 import fs from 'fs';
 import http from 'http';
+import rp from 'request-promise';
+import Promise from 'bluebird';
 
-function getPromise(path) {
-  return new Promise((fullfill, reject) => {
-    http.get(`http://localhost:3000/${path}`, response => {
-      let body = '';
-      if (response.statusCode != 200) {
-        reject(`http error: ${path}`);
-        return;
-      }
-      response.on('data', chunk => {
-        body += chunk;
-      });
-      response.on('end', () => {
-        const result = JSON.parse(body);
-        fullfill(result);
-      });
-    });
-  });
-}
-
-function readPromise(file) {
-  return new Promise((fullfill, reject) => {
-    fs.readFile(file, (err, data) => {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      fullfill(JSON.parse(data));
-    });
-  });
-}
+Promise.promisifyAll(fs);
 
 async function report() {
   try {  
-    const user = await getPromise('user');
-    const details = await readPromise(`${user.name}.json`);
-    const result = await getPromise(`reports/${user.name}?secret=${details.secret}`);
+    const user = JSON.parse(await rp(`http://localhost:3000/user`));
+    const details = JSON.parse(await fs.readFileAsync(`${user.name}.json`));
+    const result = JSON.parse(await rp(`http://localhost:3000/reports/${user.name}?secret=${details.secret}`));
     console.log(result);
   } catch (e) {
     console.log(e);
